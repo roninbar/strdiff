56bc67ad3f769fbd3951f2b27536f4cae0acf097
ron <rinbar70@gmail.com>
2020-08-27 13:47:52 +0300

    Whitespace.

--- multistrdiff.c
+++ multistrdiff.c
@@ -6,12 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-	char str1[STRLEN+1], str2[STRLEN+1];
+	char str1[STRLEN + 1], str2[STRLEN + 1];
 
 	while ((mygets(str1, STRLEN) != NULL) && (mygets(str2, STRLEN) != NULL))
 	{
 		printf("%d\n", mystrdiff(str1, str2));
-	}	
+	}
 	return 0;
 }
 
